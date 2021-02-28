e9a50409cf4ae9f9a5677a000734a1bf09535172
swade781977 <48293986swade781977@users.noreply.github.com>
2021-02-28 17:01:42 -0500

    first commit

---
+++ db/migrate/20210228220031_create_games.rb
@@ -0,0 +1,12 @@
+class CreateGames < ActiveRecord::Migration[6.1]
+  def change
+    create_table :games do |t|
+      t.references :user, null: false, foreign_key: true
+      t.integer :score
+      t.integer :lines
+      t.integer :level
+
+      t.timestamps
+    end
+  end
+end
